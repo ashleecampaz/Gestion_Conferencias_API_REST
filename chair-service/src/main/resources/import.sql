ff886b748f4a703d46e99bace95de3faf838fbdd
Ashlee Campaz <ashleecampaz@unicauca.edu.co>
2024-10-11 16:19:59 -0500

    Añadi un conjunto de insert sql

---
+++ chair-service/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Chair(name,lastName, email) values('Ashlee','Campaz','ashleecampaz@gmail.com');
+INSERT INTO Chair(name,lastName, email) values('Lina','Diaz','linaDiaz@gmail.com');
+INSERT INTO Chair(name,lastName, email) values('Karen', 'Snadoval','ksandoval@gmail.com');
